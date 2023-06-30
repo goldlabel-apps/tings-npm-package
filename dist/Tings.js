c441bc0793f47e05167a0e95181da7c786659657
listingslab <listingslab@gmail.com>
2023-06-30 12:57:35 +0200

    Staging (#5)
    * ...
    
    * Update Tings.tsx
    
    * Tings

--- dist/Tings.js
+++ dist/Tings.js
@@ -1,6 +1,3 @@
-import * as React from "react";
 export function Tings() {
-    return (React.createElement("div", null,
-        React.createElement("div", { style: { background: "gold" } },
-            React.createElement("h4", null, "Tings"))));
+    return null;
 }
