a5122bb8e2d27b80335fc63eae3607da9df5571a
milky <listingslab@gmail.com>
2023-06-30 12:50:35 +0200

    Update Tings.tsx

--- src/Tings.tsx
+++ src/Tings.tsx
@@ -1,11 +1,5 @@
 import * as React from "react"
 
 export function Tings() {
-  return (
-    <div>
-        <div style={{background:"gold"}}>
-          <h4>Tings</h4>
-        </div>
-    </div>
-  )
+  return null
 }
